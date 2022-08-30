356f6e5813e069dc610aef3e8f9bbd7f49c2cf4b
Bruno <brunobonil@gmail.com>
2022-08-30 18:07:12 -0300

    Avances en Anchura

--- tp1-puzzle/8_puzzle.py
+++ tp1-puzzle/8_puzzle.py
@@ -1,29 +1,42 @@
+from lib2to3.pytree import Node
 from random import randint
 from copy import deepcopy
 
-puzzle = [[1, 2, 3], 
-          [4, 5, 6], 
-          [7, 8, 'X']]
 
-def finder(): # Devuelve lista con la posición de 'X' Ej: [2, 2]
-    for i in puzzle:
+class Nodo:
+    def __init__(self):
+        self.padre = None
+        self.hijos = []
+        self.tabla = None
+
+    def agregar_hijos(self, nodo):
+        self.tabla.append(nodo)
+
+    
+        
+# puzzle = [[1, 2, 3], 
+#           [4, 5, 6], 
+#           [7, 8, 'X']]
+puzzle = [['X', 2, 3], [1, 8, 5], [4, 7, 6]]
+
+def finder(board): # Devuelve lista con la posición de 'X' Ej: [2, 2]
+    for i in board:
         if 'X' in i:
-            return [puzzle.index(i), i.index('X')]
+            return [board.index(i), i.index('X')]
 
-def moves(dir, fila, col):
+def moves(board, dir, fila, col):
     
     if dir == 1: #IZQUIERDA
-        puzzle[fila][col], puzzle[fila][col-1] = puzzle[fila][col-1], puzzle[fila][col]
+        board[fila][col], board[fila][col-1] = board[fila][col-1], board[fila][col]
     if dir == 2: #DERECHA
-        puzzle[fila][col], puzzle[fila][col+1] = puzzle[fila][col+1], puzzle[fila][col]
+        board[fila][col], board[fila][col+1] = board[fila][col+1], board[fila][col]
     if dir == 3: #ARRIBA
-        puzzle[fila][col], puzzle[fila-1][col] = puzzle[fila-1][col], puzzle[fila][col]
+        board[fila][col], board[fila-1][col] = board[fila-1][col], board[fila][col]
     if dir == 4: #ABAJO
-        puzzle[fila][col], puzzle[fila+1][col] = puzzle[fila+1][col], puzzle[fila][col]
+        board[fila][col], board[fila+1][col] = board[fila+1][col], board[fila][col]
 
-def possible_moves():
-    fila = finder()[0]
-    col = finder()[1]
+def possible_moves(board):
+    fila, col = finder(board) 
     pos_moves = [] 
     if col != 0:
         pos_moves.append(1)
@@ -33,34 +46,48 @@ def possible_moves():
         pos_moves.append(3)
     if fila != 2:
         pos_moves.append(4)
-    return [pos_moves, [fila, col]]
+    return pos_moves
 
-def mix():
+def mix(board):
     for i in range(20):
-        poss_moves = possible_moves()[0]
+        poss_moves = possible_moves()[0] # Retorna lista con posibles mov y posición
         fila = possible_moves()[1][0]
         col = possible_moves()[1][1]
         ran_move = randint(0, len(poss_moves)-1)
         dir = poss_moves[ran_move]
-        moves(dir, fila, col)
+        moves(board, dir, fila, col)
 
-def solución_random():
+def solución_random(board):
     counter = 0
     while puzzle != [[1,2, 3], [4, 5, 6], [7, 8, 'X']]:
-        mix()
+        mix(board)
         counter += 1
     return counter
 
-def solución_anchura():
-    inicial = puzzle
+def solución_anchura(board):     # puzzle = [['X', 2, 3], [1, 8, 5], [4, 7, 6]]
+    poss_moves = possible_moves()[0]
+    fila = possible_moves()[1]
+    col = possible_moves()[2]
+    padre = Nodo()
+    padre.tabla = board
     
 
-mix()
+    
+
+#mix()
 for i in puzzle:
     print(i)
 print('\n')
 
 
+solución_anchura(puzzle)
+
 #print(f"Número de movimientos: {solución_random()}")
+
+# for i in puzzle:
+#     print(i)
+# print('\n')
+
+
 
 
